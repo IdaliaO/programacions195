c58a23223a379bea7cd476065e73185dce010d41
IdaliaO <157184583+IdaliaO@users.noreply.github.com>
2024-11-04 18:05:54 -0600

    avance 6:05

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -5,16 +5,13 @@ use App\Http\Controllers\ControladorVistas;
 
 // rutas para trabajar con un controlador
 
-<<<<<<< HEAD
 Route::get('/', [ControladorVistas::class, 'home'])->name('rutainicio');
 Route::get('/formulario', [ControladorVistas::class, 'formulario'])->name('rutaformulario');
 Route::get('/clientes', [ControladorVistas::class, 'consulta'])->name('rutaclientes');
 Route::post('/enviar', [ControladorVistas::class, 'procesarCliente'])->name('procesar');
-=======
 Route :: view('/','inicio');
 Route :: view('/formulario','formulario');
 Route :: view('/clientes','clientes');
->>>>>>> parent of 8a6ff7c (completo 3.1)
 
 
 
